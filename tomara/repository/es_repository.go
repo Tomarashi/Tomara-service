e12fc067332459bfae56e8812de12e78478432d7
Giorgi Megrelishvili <gmegr18@freeuni.edu.ge>
2023-08-25 13:28:41 +0400

    Implemented elasticsearch repository

--- tomara/repository/es_repository.go
+++ tomara/repository/es_repository.go
@@ -1,39 +1,151 @@
 package repository
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
+	"golang.org/x/net/context"
+	"io"
+)
+
+const (
+	anyCharRegex     = ".*"
+	wordGeoFieldName = "word_geo"
 )
 
 type ElasticSearchRepository struct {
-	Client elasticsearch.Client
+	client *elasticsearch.Client
 }
 
-func createQuery(fieldName string, resultSize int) string {
-	return fmt.Sprintf(`{
-		"query": {
-			"regexp": {
-				"%s": {
+type buildQueryOptions struct {
+	fieldName         string
+	subValue          string
+	useOnlyStartsWith bool
+	resultSize        int
+}
+
+func buildQuery(opts buildQueryOptions) (error, []byte) {
+	var buf bytes.Buffer
+	var valueRegex string
+	if opts.useOnlyStartsWith {
+		valueRegex = opts.subValue + anyCharRegex
+	} else {
+		valueRegex = anyCharRegex + opts.subValue + anyCharRegex
+	}
+
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"regexp": map[string]interface{}{
+				opts.fieldName: map[string]interface{}{
+					"value": valueRegex,
 					"flags": "ALL",
-					"value": "ჰ.*"
-				}
-			}
+				},
+			},
 		},
-		"size": %d,
-		"sort": [{
-			"frequency": {
-				"order": "desc"
-			}
-		}]
-	}`, fieldName, resultSize)
+		"sort": []map[string]interface{}{
+			{
+				"frequency": map[string]string{
+					"order": "desc",
+				},
+			},
+		},
+		"size": opts.resultSize,
+	}
+
+	if err := json.NewEncoder(&buf).Encode(query); err != nil {
+		return err, nil
+	}
+	return nil, buf.Bytes()
+}
+
+func (e *ElasticSearchRepository) makeRequest(queryBytes []byte) (error, []string) {
+	castErrorFmt := func(fieldName string) error {
+		return fmt.Errorf("can't cast '%s'", fieldName)
+	}
+
+	request := esapi.SearchRequest{
+		Index:      []string{"words"},
+		Body:       bytes.NewReader(queryBytes),
+		FilterPath: []string{"hits.hits._source.word_geo", "took"},
+	}
+	response, err := request.Do(context.Background(), e.client)
+	if err != nil {
+		return err, nil
+	}
+
+	bodyBytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return err, nil
+	}
+	err = response.Body.Close()
+	if err != nil {
+		return err, nil
+	}
+
+	bodyMap := make(map[string]interface{})
+	if err := json.Unmarshal(bodyBytes, &bodyMap); err != nil {
+		return err, nil
+	}
+
+	hitsMap, castOk := bodyMap["hits"].(map[string]interface{})
+	if !castOk {
+		return castErrorFmt("hits"), nil
+	}
+
+	sourceList, castOk := hitsMap["hits"].([]interface{})
+	if !castOk {
+		return castErrorFmt("hits.hits"), nil
+	}
+
+	result := make([]string, 0, len(sourceList))
+	for i, sourceElem := range sourceList {
+		sourceMap, castOk := sourceElem.(map[string]interface{})
+		if !castOk {
+			return castErrorFmt(fmt.Sprintf("hits.hits[%d]", i)), nil
+		}
+		val, castOk := sourceMap["_source"].(map[string]interface{})
+		if !castOk {
+			return castErrorFmt(fmt.Sprintf("hits.hits[%d]_source", i)), nil
+		}
+		result = append(result, val["word_geo"].(string))
+	}
+	return nil, result
+}
+
+func (e *ElasticSearchRepository) findWords(substring string, limit int, useOnlyStartsWith bool) (error, []string) {
+	err, queryBytes := buildQuery(buildQueryOptions{
+		fieldName:         wordGeoFieldName,
+		resultSize:        limit,
+		subValue:          substring,
+		useOnlyStartsWith: useOnlyStartsWith,
+	})
+	if err != nil {
+		return err, nil
+	}
+
+	err, result := e.makeRequest(queryBytes)
+	if err != nil {
+		return err, nil
+	}
+	return nil, result
 }
 
 func (e *ElasticSearchRepository) GetWordsStartsWith(substring string, limit int) (error, []string) {
-	//TODO implement me
-	panic(NotImplementedErrorString)
+	return e.findWords(substring, limit, true)
 }
 
 func (e *ElasticSearchRepository) GetWordsContains(substring string, limit int) (error, []string) {
-	//TODO implement me
-	panic(NotImplementedErrorString)
+	return e.findWords(substring, limit, false)
+}
+
+func MakeElasticSearchRepository() *ElasticSearchRepository {
+	repository := &ElasticSearchRepository{}
+	client, err := elasticsearch.NewDefaultClient()
+	if err != nil {
+		panic(err)
+	}
+	repository.client = client
+	return repository
 }
