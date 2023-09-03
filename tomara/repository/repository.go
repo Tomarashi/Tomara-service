bdcda518e403245b7c3d8a3e8c730ca49ce21fc7
Giorgi Megrelishvili <gmegr18@freeuni.edu.ge>
2023-09-03 21:15:56 +0400

    Small changes in repository searching

--- tomara/repository/repository.go
+++ tomara/repository/repository.go
@@ -1,13 +1,25 @@
 package repository
 
+import (
+	"fmt"
+)
+
+func logRepoError(dbType string, err error) {
+	fmt.Printf("Can't connect to database '%s'. Error: %s\n", dbType, err.Error())
+}
+
 func GetFirstValidRepository() ITomaraRepository {
 	esRepo := MakeElasticSearchRepository()
-	if valid, _ := esRepo.CheckDatabase(); valid {
+	if valid, err := esRepo.CheckDatabase(); valid {
 		return esRepo
+	} else {
+		logRepoError("ElasticSearch", err)
 	}
 	mySqlRepo := MakeMySqlRepositoryDefaultConfig()
-	if valid, _ := mySqlRepo.CheckDatabase(); valid {
+	if valid, err := mySqlRepo.CheckDatabase(); valid {
 		return mySqlRepo
+	} else {
+		logRepoError("MySql", err)
 	}
 	return MakeFakeRepositoryFromFile("data/dev-words.txt")
 }
